250754a75aa395e45fb471e1eb061135cb24c01a
FrikoGad <goliaf551@mail.ru>
2022-08-31 22:17:26 +0300

    Fragments

---
+++ app/src/main/java/com/example/fitnessapp/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fitnessapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fitnessapp.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}
