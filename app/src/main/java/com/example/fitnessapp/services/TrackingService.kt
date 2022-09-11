6fa838faa152e3a19a7e0b70b68bf5936a271a5f
FrikoGad <goliaf551@mail.ru>
2022-09-11 13:53:49 +0300

    Foreground service

--- app/src/main/java/com/example/fitnessapp/services/TrackingService.kt
+++ app/src/main/java/com/example/fitnessapp/services/TrackingService.kt
@@ -1,10 +1,24 @@
 package com.example.fitnessapp.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.example.fitnessapp.R
+import com.example.fitnessapp.ui.MainActivity
 import com.example.fitnessapp.util.Const.ACTION_PAUSE_SERVICE
+import com.example.fitnessapp.util.Const.ACTION_SHOW_TRACKING_FRAGMENT
 import com.example.fitnessapp.util.Const.ACTION_START_OR_RESUME_SERVICE
 import com.example.fitnessapp.util.Const.ACTION_STOP_SERVICE
+import com.example.fitnessapp.util.Const.NOTIFICATION_CHANNEL_ID
+import com.example.fitnessapp.util.Const.NOTIFICATION_CHANNEL_NAME
 import timber.log.Timber
 
 class TrackingService : LifecycleService() {
@@ -26,4 +40,38 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun startForegroundService() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+        as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
 }
