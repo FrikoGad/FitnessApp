9944aee36d6175bf9bc651cc571859f3f683fdd4
FrikoGad <goliaf551@mail.ru>
2022-10-12 21:21:10 +0300

    Setup adapter

--- app/src/main/java/com/example/fitnessapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/fitnessapp/ui/fragments/RunFragment.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitnessapp.R
+import com.example.fitnessapp.adapters.RunAdapter
 import com.example.fitnessapp.ui.viewmodels.MainViewModel
 import com.example.fitnessapp.util.Const.REQUEST_CODE_LOCATION_PERMISSION
 import com.example.fitnessapp.util.TrackingUtility
@@ -21,12 +24,26 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
 
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var runAdapter: RunAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requestPermissions()
+        setupRecyclerView()
+
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            runAdapter.submitList(it)
+        })
+
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun setupRecyclerView() = rvRuns.apply {
+        runAdapter = RunAdapter()
+        adapter = runAdapter
+        layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun requestPermissions() {
